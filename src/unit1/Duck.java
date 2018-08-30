aabe131f8f334ef9c64febb9769c3715a3eeae04
Flynn <1404009673@qq.com>
2018-08-30 21:34:37 +0800

    添加设计模式说明的注释

--- src/unit1/Duck.java
+++ src/unit1/Duck.java
@@ -1,5 +1,8 @@
 package unit1;
-
+/*
+ * 策略模式定义了算法族，分别封装起来，让它们之间可以相互替换，此模式让算法的变化独立于使用算法的客户。
+ * （封装：每个对象都包含它能进行操作的所有信息，这个特性称为封装，因此对象就不必依赖其他对象来完成操作）
+ */
 public abstract class Duck {
 	FlyBehavior flyBehavior;
 	QuackBehavior quackBehavior;
@@ -9,7 +12,7 @@ public abstract class Duck {
 	public void performFly() {
 		flyBehavior.fly();
 	}
-	public void performQuack() {
+	public void performQuack()  {
 		quackBehavior.quack();
 	}
 	public void setFlyBehavior(FlyBehavior fb) {
